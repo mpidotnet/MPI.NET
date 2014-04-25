a376642815bf2bb36b6656d1e1cd44a6bea5a6da
J-M <jean-michel.perraud@csiro.au>
2014-04-25 14:12:10 +1000

    Package 1.2.0 for Windows. Put on NuGet

--- MPI/Properties/AssemblyInfo.cs
+++ MPI/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Message Passing Interface")]
+[assembly: AssemblyTitle("Message Passing Interface for .NET")]
 [assembly: AssemblyDescription("A library for writing parallel, distributed applications for clusters.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Indiana University")]
 [assembly: AssemblyProduct("MPI")]
-[assembly: AssemblyCopyright("Copyright © 2007, 2008 The Trustees of Indiana University")]
+[assembly: AssemblyCopyright("Copyright © 2007, 2008 The Trustees of Indiana University; Contributions Copyright © 2013 CSIRO")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.2.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2")]
